8b189e81f06086e08421cdae3056e6cd7e22fac3
Никита Боронин <nikitab157@outlook.com>
2024-11-21 22:39:34 +0300

    Добавьте файлы проекта.

---
+++ Homework_4/Program.cs
@@ -0,0 +1,283 @@
+﻿namespace Homework_4
+{
+    internal class Program
+    {
+        // Вывести на экран массив из 20 случайных чисел. Ввести два числа из этого массива,
+        // которые нужно поменять местами.Вывести на экран получившийся массив.
+        // 1 Задание!
+        static bool NumberExistsInArray(int[] array, int number)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static int FindIndex(int[] array, int number)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1; 
+        }
+        static void Taks1()
+        {
+            Console.WriteLine("1 задача\n");
+            Random random = new Random();
+            int[] numbers = new int[20];
+
+           
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(0, 100);
+                Console.WriteLine($"Значение массива на месте [{i}] - {numbers[i]}");
+            }
+
+            Console.WriteLine("Введите два числа из массива, которые хотите поменять местами. У вас есть 2 попытки.");
+
+            int swapCount = 0; 
+
+            while (swapCount < 2)
+            {
+                Console.WriteLine("Введите первое число:");
+                if (!int.TryParse(Console.ReadLine(), out int num1) || !NumberExistsInArray(numbers, num1))
+                {
+                    Console.WriteLine("Первое число отсутствует в массиве. Попробуйте снова.");
+                    continue;
+                }
+
+                Console.WriteLine("Введите второе число:");
+                if (!int.TryParse(Console.ReadLine(), out int num2) || !NumberExistsInArray(numbers, num2))
+                {
+                    Console.WriteLine("Второе число отсутствует в массиве. Попробуйте снова.");
+                    continue;
+                }
+
+                
+                int index1 = FindIndex(numbers, num1);
+                int index2 = FindIndex(numbers, num2);
+
+                
+                Console.WriteLine($"Меняем местами числа {num1} и {num2}.");
+                numbers[index1] = num2;
+                numbers[index2] = num1;
+
+                swapCount++;
+            }
+
+            
+            Console.WriteLine("Итоговый массив:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Значение массива на месте [{i}] - {numbers[i]}");
+            }
+
+        }
+
+        // 2 задание
+        static int Task2(ref int product, out double average, params int[] array)
+        {
+            Console.WriteLine("2 задание\n");
+            
+            if (array.Length == 0)
+            {
+                Console.WriteLine("Массив пуст. Невозможно выполнить вычисления.");
+                product = 0;
+                average = 0;
+                return 0;
+            }
+
+            int sum = 0;
+            product = 1;
+
+            foreach (int num in array)
+            {
+                sum += num;
+                product *= num;
+            }
+
+            average = (double)sum / array.Length;
+
+            Console.WriteLine($"Сумма элементов массива: {sum}");
+            Console.WriteLine($"Произведение элементов массива: {product}");
+            Console.WriteLine($"Среднее арифметическое элементов массива: {average}");
+
+            return sum;
+
+        }
+
+        // 3 Задача!
+        static void DrawNumber(int number)
+        {
+            
+            string[] patterns = new string[]
+            {
+            "#####\n#   #\n#   #\n#   #\n#####", // 0
+            "  #  \n  #  \n  #  \n  #  \n  #  ", // 1
+            "#####\n    #\n#####\n#    \n#####", // 2
+            "#####\n    #\n#####\n    #\n#####", // 3
+            "#   #\n#   #\n#####\n    #\n    #", // 4
+            "#####\n#    \n#####\n    #\n#####", // 5
+            "#####\n#    \n#####\n#   #\n#####", // 6
+            "#####\n    #\n    #\n    #\n    #", // 7
+            "#####\n#   #\n#####\n#   #\n#####", // 8
+            "#####\n#   #\n#####\n    #\n#####", // 9
+            };
+
+            
+            Console.WriteLine(patterns[number]);
+        }
+        static void Task3()
+        {
+            Console.WriteLine("3 задание\n");
+            while (true)
+            {
+                Console.WriteLine("Введите число от 0 до 9 или команду 'exit'/'закрыть' для выхода:");
+
+                string input = Console.ReadLine();
+
+                // Проверка на выход
+                if (input.ToLower() == "exit" || input.ToLower() == "закрыть")
+                {
+                    Console.WriteLine("Программа завершена.");
+                    break;
+                }
+
+                try
+                {
+                    // Проверка на число
+                    int number = int.Parse(input);
+
+                    if (number >= 0 && number <= 9)
+                    {
+                        DrawNumber(number);
+                    }
+                    else
+                    {
+                        // Число не в диапазоне от 0 до 9
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ошибка: число должно быть от 0 до 9!");
+                        Thread.Sleep(3000);
+                        Console.ResetColor();
+                    }
+                }
+                catch (FormatException)
+                {
+                    // Введено не число
+                    Console.WriteLine("Ошибка: введено не число! Программа завершена с исключением.");
+                    throw;
+                }
+            }
+        }
+
+        // 4 Задача!
+
+        struct GrandFather
+        {
+            public string Name; // Имя деда
+            public GrumbleLevel Level; // Уровень ворчливости
+            public string[] Phrases; // Массив фраз для ворчания
+            public int NumberBruises; // Количество фингалов от бабки
+
+            // Метод, который проверяет наличие матерных слов в фразах деда
+            public int GetHit(params string[] swearWords)
+            {
+                foreach (var phrase in Phrases)
+                {
+                    foreach (var swear in swearWords)
+                    {
+                        if (phrase.ToLower().Contains(swear.ToLower()))
+                        {
+                            NumberBruises++;
+                        }
+                    }
+                }
+                Console.WriteLine($"Дед {Name} получил {NumberBruises} фингалов.");
+                return NumberBruises;
+            }
+        }
+
+        enum GrumbleLevel
+        {
+            Low = 1,
+            Medium,
+            High
+        }
+
+        static void Main(string[] args)
+        {
+
+            Taks1();
+            int number = 10;
+            double average;
+            int[] numbers = { 1, 2 };
+            Task2(ref number, out average, numbers);
+            Task3();
+
+            Console.WriteLine("\n 4 задание\n");
+            GrandFather[] grandFathers = new GrandFather[5];
+
+            grandFathers[0] = new GrandFather
+            {
+                Name = "Иван",
+                Level = GrumbleLevel.Low,
+                Phrases = new string[] { "Черт!", "Какого черта!", "Гады!" },
+                NumberBruises = 0
+            };
+
+            grandFathers[1] = new GrandFather
+            {
+                Name = "Петр",
+                Level = GrumbleLevel.Medium,
+                Phrases = new string[] { "Шкет!", "Дурень!", "Черт побери!" },
+                NumberBruises = 0
+            };
+
+            grandFathers[2] = new GrandFather
+            {
+                Name = "Алексей",
+                Level = GrumbleLevel.High,
+                Phrases = new string[] { "Имбицил!", "Дебил!", "Это просто ужас!" },
+                NumberBruises = 0
+            };
+
+            grandFathers[3] = new GrandFather
+            {
+                Name = "Михаил",
+                Level = GrumbleLevel.Low,
+                Phrases = new string[] { "Кто тут?", "Гады!" },
+                NumberBruises = 0
+            };
+
+            grandFathers[4] = new GrandFather
+            {
+                Name = "Семен",
+                Level = GrumbleLevel.Medium,
+                Phrases = new string[] { "Блин!", "Черт!", "Шимпанзе!" },
+                NumberBruises = 0
+            };
+
+            
+            string[] swearWords = { "блин", "черт", "шимпанзе", "гады", "дебил", "имбицил", "Дурень" };
+
+            
+            foreach (var grandfather in grandFathers)
+            {
+                grandfather.GetHit(swearWords);
+            }
+
+        }
+        
+        
+
+
+    }
+}
---
+++ Tumakov_lab_09-421_BoroninNikita/Program.cs
@@ -0,0 +1,217 @@
+﻿namespace Tumakov_lab_09_421_BoroninNikita
+{
+    internal class Program
+    {
+        // 1 Задание!
+        static int GetMax(int a, int b)
+        {
+            Console.WriteLine("Упражнение 5.1");
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        // 2 Задание!
+        static void Swap(ref int a, ref int b)
+        {
+            
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // 3 Задание!
+        static bool Factorial(int n, out ulong result)
+        {
+            result = 1;
+
+            
+            if (n < 0)
+            {
+                Console.WriteLine("Факториал отрицательного числа не существует.");
+                result = 0;
+                return false;
+            }
+
+            
+            if (n > 20)
+            {
+                Console.WriteLine("Факториал слишком большого числа не может быть вычислен из-за переполнения.");
+                result = 0;
+                return false;
+            }
+
+            try
+            {
+                checked
+                {
+                    for (int i = 1; i <= n; i++)
+                    {
+                        result *= (ulong)i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                result = 0;
+                return false;
+            }
+        }
+
+        // 4 Задание!
+        static bool RecursiveFactorial(int n, out ulong result)
+        {
+            result = 1;
+
+            
+            if (n < 0)
+            {
+                Console.WriteLine("Факториал отрицательного числа не существует.");
+                result = 0;
+                return false;
+            }
+
+            
+            if (n > 20)
+            {
+                Console.WriteLine("Факториал слишком большого числа не может быть вычислен из-за переполнения.");
+                result = 0;
+                return false;
+            }
+
+            try
+            {
+                checked
+                {
+                    
+                    result = CalculateFactorial(n);
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                result = 0; 
+                return false;
+            }
+        }
+
+
+        private static ulong CalculateFactorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1; 
+
+            return (ulong)n * CalculateFactorial(n - 1); 
+        }
+
+        // 5 Task
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        
+        static int GCD(int a, int b, int c)
+        {
+            return GCD(GCD(a, b), c); 
+        }
+
+
+        // 6 Task
+        static long Fibonacci(int n)
+        {
+            if (n <= 1) // Базовый случай: F(0) = 0, F(1) = 1
+            {
+                return n;
+            }
+            else
+            {
+                // Рекурсивный вызов: F(n) = F(n-1) + F(n-2)
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+           }
+        }
+
+        static void Main(string[] args)
+        {
+            // 1 Task
+            int num1 = 5;
+            int num2 = 10;
+
+            int max = GetMax(num1, num2);
+            Console.WriteLine($"Наибольшее число из {num1} и {num2} = {max}");
+
+
+            // 2 Task
+           
+            Console.WriteLine("\nУпражнение 5.2");
+            Console.WriteLine($"До обмена: num1 = {num1}, num2 = {num2}");
+
+          
+            Swap(ref num1, ref num2);
+            Console.WriteLine($"После обмена: num1 = {num1}, num2 = {num2}");
+
+
+            // 3 Task
+            Console.WriteLine("\nУпражнение 5.3");
+            int number = 2; // Пример числа для вычисления факториала
+            ulong resultTT;
+            bool success = Factorial(number, out resultTT);
+
+            if (success)
+            {
+                Console.WriteLine($"Факториал числа {number} равен {resultTT}");
+            }
+            else
+            {
+                Console.WriteLine($"Произошло переполнение при вычислении факториала числа {number}");
+            }
+
+
+            // 4 Task
+            Console.WriteLine("\nУпражнение 5.4");
+            if (RecursiveFactorial(5, out ulong result))
+            {
+                Console.WriteLine($"Факториал числа 5: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка при вычислении факториала.");
+            }
+
+
+            // 5 Task
+            Console.WriteLine("\nДомашнее задание 5.1");
+            int nums1 = 56, nums2 = 98;
+            int num3 = 42;
+
+            
+            int result2 = GCD(nums1, nums2);
+            Console.WriteLine($"НОД чисел {nums1} и {nums2} = {result2}");
+
+            
+            int result3 = GCD(nums1, nums2, num3);
+            Console.WriteLine($"НОД чисел {nums1}, {nums2} и {num3} = {result3}");
+
+
+            // 6 Task
+            Console.WriteLine("\nДомашнее задание 5.2");
+            int n = 10; // Вы можете изменить это значение для получения другого числа ряда Фибоначчи
+            long resultss = Fibonacci(n);
+
+            Console.WriteLine($"Число Фибоначчи для n = {n} равно {resultss}");
+
+        }
+    }
+}
